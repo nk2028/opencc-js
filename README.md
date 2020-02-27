3e1319f04ce0fe8c9314a50dc99564fc44bd021e
sgalal <32255369+chromezh@users.noreply.github.com>
2020-02-27 19:33:32 +0800

    Package OpenCC 2

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ JavaScript implementation of OpenCC 2
 ## Usage
 
 ```javascript
->>> cc = await Converter.create({ from_variant: 'hk', to_variant: 'cn' });
+>>> cc = await OpenCC2.Converter.create({ from_variant: 'hk', to_variant: 'cn' });
 >>> cc.convert('政府初步傾向試驗為綠色專線小巴設充電裝置')
 "政府初步倾向试验为绿色专线小巴设充电装置"
 ```
+
+## API
+
+`OpenCC2.Converter.create(config)`:
+
+`config` is a JavaScript Object which contains `from_variant` and `to_variant`.
+
+Possible values are: `cn`, `hk`, `tw` and `t`. `t` stands for Traditional Chinese (OpenCC 2 standard).
--- index.js
+++ index.js
@@ -1,143 +1,143 @@
-/* Trie */
+'use strict';
 
-function makeEmptyTrie() {
-	return [{} /* child nodes */ /* , v */];
-}
+const OpenCC2 = {
+	/* Trie */
 
-function addWord(t, s, v) {
-	for (const c of s) {
-		const nodes = t[0];
-		if (!(c in nodes)) {
-			nodes[c] = makeEmptyTrie();
+	_makeEmptyTrie: () => {
+		return [{} /* child nodes */ /* , v */];
+	},
+
+	_addWord: (t, s, v) => {
+		for (const c of s) {
+			const nodes = t[0];
+			if (!(c in nodes)) {
+				nodes[c] = OpenCC2._makeEmptyTrie();
+			}
+			t = nodes[c];
 		}
-		t = nodes[c];
-	}
-	t.push(v);
-}
+		t.push(v);
+	},
 
-function hasValue(t) {
-	return t.length == 2;
-}
+	_hasValue: t => {
+		return t.length == 2;
+	},
 
-function longestPrefix(t, s) {
-	const res = [];
-	let cur = [], target;
-	for (const c of s) {
-		cur.push(c);
-		const nodes = t[0];
-		if (!(c in nodes))
-			break;
-		t = nodes[c];
-		if (hasValue(t)) {
-			target = t[1];
-			res.push(...cur);
-			cur = [];
+	_longestPrefix: (t, s) => {
+		const res = [];
+		let cur = [], target;
+		for (const c of s) {
+			cur.push(c);
+			const nodes = t[0];
+			if (!(c in nodes))
+				break;
+			t = nodes[c];
+			if (OpenCC2._hasValue(t)) {
+				target = t[1];
+				res.push(...cur);
+				cur = [];
+			}
 		}
-	}
-	if (res.length)
-		return [res.join(''), target];  // k, v
-}
+		if (res.length)
+			return [res.join(''), target];  // k, v
+	},
 
-function _test() {
-	const t = makeEmptyTrie();
-	addWord(t, 'c', 'aaa');
-	addWord(t, 'd', 'bbb');
-	addWord(t, 'da', 'ccc');
-	console.log(longestPrefix(t, 'c'), ['c', 'aaa']);
-	console.log(longestPrefix(t, 'cc'), ['c', 'aaa']);
-	console.log(longestPrefix(t, 'dccc'), ['d', 'bbb']);
-	console.log(longestPrefix(t, 'dacc'), ['da', 'ccc']);
-}
+	/* Load dict */
 
-// _test();
+	_load_dict: async (s, type) => {
+		const DICT_ROOT = 'https://sgalal.github.io/opencc2-dict/data/';
 
-/* Load dict */
+		const DICT_FROM = { 'cn': ['STCharacters', 'STPhrases']
+			, 'hk': ['HKVariantsRev', 'HKVariantsRevPhrases']
+			, 'tw': ['TWVariantsRev', 'TWVariantsRevPhrases']
+			, 'twp': ['TWVariantsRev', 'TWVariantsRevPhrases', 'TWPhrasesRev']
+			, 'jp': ['JPVariantsRev']
+			},
+			DICT_TO = { 'cn': ['TSCharacters', 'TSPhrases']
+			, 'hk': ['HKVariants', 'HKVariantsPhrases']
+			, 'tw': ['TWVariants']
+			, 'twp': ['TWVariants', 'TWPhrasesIT', 'TWPhrasesName', 'TWPhrasesOther']
+			, 'jp': ['JPVariants']
+			};
 
-async function load_dict(s, type) {
-	const DICT_ROOT = 'https://sgalal.github.io/opencc2-dict/data/';
+		async function getDictText(url) {
+			const response = await fetch(DICT_ROOT + url + '.txt');
+			const mytext = await response.text();
+			return mytext;
+		}
 
-	const DICT_FROM = { 'cn': ['STCharacters', 'STPhrases']
-		, 'hk': ['HKVariantsRev', 'HKVariantsRevPhrases']
-		, 'tw': ['TWVariantsRev', 'TWVariantsRevPhrases']
-		, 'twp': ['TWVariantsRev', 'TWVariantsRevPhrases', 'TWPhrasesRev']
-		, 'jp': ['JPVariantsRev']
-		},
-		DICT_TO = { 'cn': ['TSCharacters', 'TSPhrases']
-		, 'hk': ['HKVariants', 'HKVariantsPhrases']
-		, 'tw': ['TWVariants']
-		, 'twp': ['TWVariants', 'TWPhrasesIT', 'TWPhrasesName', 'TWPhrasesOther']
-		, 'jp': ['JPVariants']
-		};
+		let DICTS;
+		if (type == 'from')
+			DICTS = DICT_FROM[s];
+		else if (type == 'to')
+			DICTS = DICT_TO[s];
+		const t = OpenCC2._makeEmptyTrie();
+		for (const DICT of DICTS) {
+			const txt = await getDictText(DICT);
+			const lines = txt.split('\n');
+			for (const line of lines) {
+				if (line && !line.startsWith('#')) {
+					const [l, r] = line.split('\t');
+					OpenCC2._addWord(t, l, r.split(' ')[0]);  // 若有多個候選，只選擇第一個
+				}
+			}
+		}
+		return t;
+	},
 
-	async function getDictText(url) {
-		const response = await fetch(DICT_ROOT + url + '.txt');
-		const mytext = await response.text();
-		return mytext;
-	}
+	_convert: (t, s) => {
+		const res = [];
+		while (s.length) {
+			const prefix = OpenCC2._longestPrefix(t, s);
+			if (prefix) {
+				const [k, v] = prefix;
+				res.push(v);
+				s = s.slice(k.length);
+			} else {
+				const k = s[Symbol.iterator]().next().value;  // Unicode-aware version of s[0]
+				res.push(k);
+				s = s.slice(k.length);
+			}
+		}
+		return res.join('');
+	},
 
-	let DICTS;
-	if (type == 'from')
-		DICTS = DICT_FROM[s];
-	else if (type == 'to')
-		DICTS = DICT_TO[s];
-	const t = makeEmptyTrie();
-	for (const DICT of DICTS) {
-		const txt = await getDictText(DICT);
-		const lines = txt.split('\n');
-		for (const line of lines) {
-			if (line && !line.startsWith('#')) {
-				const [l, r] = line.split('\t');
-				addWord(t, l, r.split(' ')[0]);  // 若有多個候選，只選擇第一個
+	/* Converter */
+
+	Converter: {
+		create: async config => {
+			const dict_from = config.from_variant == 't' ? null : await OpenCC2._load_dict(config.from_variant, 'from'),
+				dict_to = config.to_variant == 't' ? null : await OpenCC2._load_dict(config.to_variant, 'to');
+			return {
+				convert: s => {
+					if (config.from_variant != 't')
+						s = OpenCC2._convert(dict_from, s);
+					if (config.to_variant != 't')
+						s = OpenCC2._convert(dict_to, s);
+					return s;
+				}
 			}
 		}
 	}
-	return t;
 }
 
-/* Converter */
-
-function length(s) {
-	return [...s].length;
+/* function _test() {
+	const t = OpenCC2._makeEmptyTrie();
+	OpenCC2._addWord(t, 'c', 'aaa');
+	OpenCC2._addWord(t, 'd', 'bbb');
+	OpenCC2._addWord(t, 'da', 'ccc');
+	console.log(OpenCC2._longestPrefix(t, 'c'), ['c', 'aaa']);
+	console.log(OpenCC2._longestPrefix(t, 'cc'), ['c', 'aaa']);
+	console.log(OpenCC2._longestPrefix(t, 'dccc'), ['d', 'bbb']);
+	console.log(OpenCC2._longestPrefix(t, 'dacc'), ['da', 'ccc']);
 }
 
-function convert(t, s) {
-	const res = [];
-	while (s.length) {
-		const prefix = longestPrefix(t, s);
-		if (prefix) {
-			const [k, v] = prefix;
-			res.push(v);
-			s = s.slice(k.length);
-		} else {
-			const k = [...s][0];
-			res.push(k);
-			s = s.slice(k.length);
-		}
-	}
-	return res.join('');
-}
+_test();
 
 function _test2() {
-	const t = makeEmptyTrie();
-	addWord(t, '𦫖', 'aaa');
-	addWord(t, '的𫟃', 'bbb');
-	console.assert(convert(t, '𦫖1的𫟃𩇩c') == 'aaa1bbb𩇩c');
+	const t = OpenCC2._makeEmptyTrie();
+	OpenCC2._addWord(t, '𦫖', 'aaa');
+	OpenCC2._addWord(t, '的𫟃', 'bbb');
+	console.assert(OpenCC2._convert(t, '𦫖1的𫟃𩇩c') == 'aaa1bbb𩇩c');
 }
 
-// _test2();
-
-const Converter = {
-	create: async config => {
-		const o = {};
-		o.dict_from = config.from_variant == 't' ? null : await load_dict(config.from_variant, 'from');
-		o.dict_to = config.to_variant == 't' ? null : await load_dict(config.to_variant, 'to');
-		o.convert = s => {
-			if (config.from_variant != 't')
-				s = convert(o.dict_from, s);
-			if (config.to_variant != 't')
-				s = convert(o.dict_to, s);
-			return s;
-		};
-		return o;
-	}
-}
+_test2(); */
