0c8bea58e472e2f69f861014b9a63e8ec9eee372
sgalal <sgalal.me@outlook.com>
2021-01-10 15:14:35 +0800

    Add speed test

---
+++ test/prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -LsSo test/神雕侠侣.txt https://raw.githubusercontent.com/weiyinfu/JinYong/db1d9a9/%E9%87%91%E5%BA%B8%E5%85%A8%E9%9B%86%E4%B8%89%E8%81%94%E7%89%88/%E7%A5%9E%E9%9B%95%E4%BE%A0%E4%BE%A3.txt
+curl -LsSo test/天龙八部.txt https://raw.githubusercontent.com/weiyinfu/JinYong/db1d9a9/%E9%87%91%E5%BA%B8%E5%85%A8%E9%9B%86%E4%B8%89%E8%81%94%E7%89%88/%E5%A4%A9%E9%BE%99%E5%85%AB%E9%83%A8.txt
